6d3a794875c02bbc6f99b9b304f24a303fe8b45b
Anastasiya <Anastasiya3161@gmail.com>
2018-08-13 12:22:22 +0300

    initial commit

---
+++ app/src/main/java/com/example/msi/paint/FingerPath.java
@@ -0,0 +1,23 @@
+package com.example.msi.paint;
+
+/**
+ * Created by msi on 10.08.2018.
+ */
+import android.graphics.Path;
+
+public class FingerPath {
+
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+}
